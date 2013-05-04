a84df3e4cbc18abd83a0bf2d3492f7c83bc9549b
igor rocha <igortice@gmail.com>
2013-05-04 13:31:11 -0300

    questão 16 ok

---
+++ problem_016.rb
@@ -0,0 +1,7 @@
+=begin
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+=end
+
+p (2**1000).to_s.chars.map(&:to_i).inject(:+)
